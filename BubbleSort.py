b3213b15e4baf45e8312c37a88a221ad2942065d
AndyTert <andy.tertzakian@gmail.com>
2014-03-31 23:34:19 -0600

    Initial commit

---
+++ BubbleSort.py
@@ -0,0 +1,23 @@
+__author__ = 'andytertzakian'
+
+##This project sorts a list of 5 numbers using a bubble sort.
+
+def bubbleSort():
+
+	a = 0
+	b = 0
+	c = 0
+	d = 0
+	e = 0
+
+	a = int(input("Please enter your first number: "))
+	b = int(input("Please enter your second number: "))
+	c = int(input("Please enter your third number: "))
+	d = int(input("Please enter your fourth number: "))
+	e = int(input("Please enter your fifth number: "))
+
+	print(a, b, c, d, e)
+
+print("Hello! Please enter five different numbers when prompted")
+
+
