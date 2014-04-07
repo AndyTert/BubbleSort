e8a924045f2a9921668001130c5a655745994fd7
AndyTert <andy.tertzakian@gmail.com>
2014-04-06 22:02:27 -0600

    Commit 2

--- BubbleSort.py
+++ BubbleSort.py
@@ -12,14 +12,37 @@ def bubbleSort():
 	d = 0
 	e = 0
 
-	a = int(input("Please enter your first number: "))
-	b = int(input("Please enter your second number: "))
-	c = int(input("Please enter your third number: "))
-	d = int(input("Please enter your fourth number: "))
-	e = int(input("Please enter your fifth number: "))
-
-	print(a, b, c, d, e)
 
 print("Hello! Please enter five different numbers when prompted")
 
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+c = int(input("Please enter your third number: "))
+d = int(input("Please enter your fourth number: "))
+e = int(input("Please enter your fifth number: "))
 
+##print(a, b, c, d, e)
+
+my_list = [a, b, c, d, e]
+
+##print(list.__len__())
+print(list)
+
+def bubble(bad_list):
+	##for i in range(list.__len__()):
+	##	for j in range(i+1, list.__len__()):
+	##		if list[j] < list[j-1]:
+	##			list[j], list[i] = list[i], list[j]
+	##i = i+1
+	length = bad_list.__len__() - 1
+	sorted = False
+
+	while not sorted :
+		sorted = True
+		for i in range(length):
+			if bad_list[i] > bad_list[i+1]:
+				sorted = False
+				bad_list[i], bad_list[i+1] = bad_list[i+1], bad_list[i]
+	print(bad_list)
+
+bubble(my_list)
